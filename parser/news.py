fcf4af38bfc263dfead10c03931086fa6aa1198d
Темирлан_Арзиев <99237891+Komandir2002@users.noreply.github.com>
2022-08-02 22:15:00 +0600

    Add files via upload
    add parser

--- parser/news.py
+++ parser/news.py
@@ -5,8 +5,8 @@ URL = "https://doramy.top/"
 
 
 HEADERS = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:97.0) Gecko/20100101 Firefox/97.0"
 }
 
 
@@ -17,15 +17,22 @@ def get_html(url, params=''):
 
 def get_data(html):
     soup = BeautifulSoup(html, "html.parser")
-    items = soup.find_all('a', class_="card-img-overlay")
+    items = soup.find_all('div', class_="item col-lg-2 col-sm-3 col-6")
     news = []
     for item in items:
         news.append({
             'title': item.find("span").getText(),
-            "link":item.find("a", class_="card-img-overlay").get("href")
+            "link":item.find("a", class_="card-img-overlay").get("href"),
+            "image": item.find('img', class_="card-img").get("src"),
+            'text':item.find("a",class_="card-img-overlay").get_text()
+
 
         })
     return news
+
+# html = get_html(URL)
+# print(get_data(html.text))
+
 
 
 def parser():
